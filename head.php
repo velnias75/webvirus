a73974ab7d0d57e7dbd2a275b225e65903e6b456
Heiko Schaefer <heiko@rangun.de>
2017-05-08 01:29:40 +0200

    Initialer Commit

---
+++ head.php
@@ -0,0 +1,15 @@
+<!docytpe html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
+<meta name="description" content="Liste der auf DVD gespeicherten Filme, Dokumentationen und Konzerten">
+<meta name="keywords" content="MP4, MKV, Filmliste, Spielfilme, Dokumentationen, Dokus, Konzerte">
+<link rel="stylesheet" href="db.css" type="text/css">
+<title>Heikos Schrott- &amp; Rentnerfilme</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require 'head.php'; ?>
+
+<table border="0" width="100%">
+  <tr><td id="layout_top" align="center" colspan="3">PLATZHALTER OBEN</td></tr>
+  <tr><td id="layout_left" valign="top">PLATZHALTER<br>LINKS</td><td id="layout_content" align="center"><?php require 'list.php'; ?></td><td id="layout_right" valign="top">PLATZHALTER<br>RECHTS</td></tr>
+  <tr><td id="layout_bottom" align="center" colspan="3">PLATZHALTER UNTEN</td></tr>
+</table>
+
+<?php require 'foot.php'; ?>
