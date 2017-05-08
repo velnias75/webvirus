4858d565c51fc0554a67c1b984db424b64c118d9
Heiko Schaefer <heiko@rangun.de>
2017-05-08 08:27:19 +0200

    moved css

--- head.php
+++ head.php
@@ -10,7 +10,7 @@
 <meta name="description" content="Liste der auf DVD gespeicherten Filme, Dokumentationen und Konzerten">
 <meta name="keywords" content="MP4, MKV, Filmliste, Spielfilme, Dokumentationen, Dokus, Konzerte">
 <link rel="stylesheet" href="//cdn.jsdelivr.net/font-hack/2.020/css/hack-extended.min.css">
-<link rel="stylesheet" href="db.css" type="text/css">
+<link rel="stylesheet" href="css/db.css" type="text/css">
 <title>Heikos Schrott- &amp; Rentnerfilme</title>
 </head>
 <body>
