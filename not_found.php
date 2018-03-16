58b91690e953805e8ea1214b75681603a23b840a
Heiko Schaefer <heiko@rangun.de>
2018-03-16 07:25:16 +0100

    added not_found.php

---
+++ not_found.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Copyright 2017 by Heiko Schäfer <heiko@rangun.de>
+ *
+ * This file is part of webvirus.
+ *
+ * webvirus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * webvirus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with webvirus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require 'head.php'; ?>
+<table id="layout" border="0" width="100%">
+<tr><td id="layout_top" valign="middle" align="center" colspan="3">
+<h1><a id="title_link" href="index.php">Heikos Schrott- &amp; Rentnerfilme</a></h1>
+<h3><span class="red_text">&#9995;</span>&nbsp;Die&nbsp;Webvirenversion&nbsp;<span class="red_text">&#9995;</span></h3></td></tr>
+<tr><td id="layout_left" align="center" valign="top">&nbsp;</td>
+<td id="layout_content" align="center" valign="top">404</td>
+<td id="layout_right" valign="top">&nbsp;</td></tr>
+<tr><td id="layout_bottom" valign="middle" align="center" colspan="3">
+<small>&copy;&nbsp;<?php echo strftime("%Y"); ?>&nbsp;by <a class="note_link" href="mailto:heiko@rangun.de?subject=Schrottfilme">Heiko Sch&auml;fer</a>
+<em>(<a class="note_link" target="_blank" href="https://github.com/velnias75/webvirus">work in progess</a>)</em></small></td></tr>
+</table>
+<?php require 'foot.php'; ?>
